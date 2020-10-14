416ea1995772226c4533cd00802171a7200e2780
unknown <PiezasCasa>
2020-10-14 20:48:58 +0200

    unzip

---
+++ project/app/routes.py
@@ -0,0 +1,35 @@
+from flask import render_template
+from app import app
+from simpleFight import Champions
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/fight')
+def fight():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    
+    return render_template('fight.html', title='fight', user=user, post=post)
